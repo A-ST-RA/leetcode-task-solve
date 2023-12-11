b12bde3417c08dd7747f2e708291df9c100ff76c
AstrA <astrax200569@gmail.com>
2023-12-11 23:35:17 +0300

    stoped task

--- src/generateParentheses/generateParentheses.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {number} n
- * @return {string[]}
- */
-const generateParenthesis = (n) => {
-  return '()';
-};
--- src/generateParentheses/generateParentheses.test.js
+++
@@ -1,7 +0,0 @@
-import { generateParenthesis } from './validParentheses';
-
-describe.only('generateParenthesis test', () => {
-  it('should return () on 1', () => {
-    expect(generateParenthesis(1)).toBe('()');
-  })
-})
